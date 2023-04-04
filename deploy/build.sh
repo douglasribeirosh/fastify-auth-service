9a3287acd326f5315a5017f019128c7393228488
Douglas Ribeiro <douglasribeiro.sh@gmail.com>
2023-04-04 13:33:38 +0200

    [FEAT] Start docker dev with empty sqlite database

--- deploy/build.sh
+++ deploy/build.sh
@@ -7,12 +7,16 @@ IMAGE_VERSION=`git show HEAD --pretty=format:"%h" --no-patch`
 NODE_VERSION=16.15.1
 NPM_VERSION=8.12.2
 PORT=8080
+DATABASE_URL="file:./dev.db"
+
 docker build .. -f app/Dockerfile \
   --target app\
   --tag $IMAGE_NAME:$IMAGE_VERSION \
   --build-arg IMAGE_VERSION=$IMAGE_VERSION \
   --build-arg NODE_VERSION=$NODE_VERSION \
   --build-arg NPM_VERSION=$NPM_VERSION \
-  --build-arg PORT=$PORT
+  --build-arg PORT=$PORT \
+  --build-arg DATABASE_URL=$DATABASE_URL
+
 docker tag $IMAGE_NAME:$IMAGE_VERSION $IMAGE_NAME
 
